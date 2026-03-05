2ba0be07f6e9baa063e1478c7cab113cb2940532
Sagarnaga-Jesus <23308060610335@cetis61.edu.mx>
2026-03-05 12:22:14 -0500

    una

---
+++ README.md
@@ -0,0 +1,4 @@
+Nombre: Jesus Antonio Sagarnaga Macias
+n.control: 23308060610335
+grado: 6
+grupo: D
---
+++ main.py
@@ -0,0 +1,41 @@
+import flet as ft
+
+def main(page: ft.Page):
+    
+    
+    
+    page.add(ft.Text("Registro de Participantes",size=30))
+    nombre=page.add(ft.TextField(label="Nombre Completo"))
+    correo=page.add(ft.TextField(label="Correo Electronico"))
+    
+    taller=page.add(ft.Dropdown(
+        options=[
+            ft.DropdownOption(key="1", text="Python para principiantes"),
+            ft.DropdownOption(key="2", text="Python para intermedio"),
+            ft.DropdownOption(key="3", text="Análisis de Datos con Pandas"),
+        ]
+    ))
+    
+    pago=page.add(ft.RadioGroup(
+        content=ft.Column([
+            ft.Radio(value="Python para principiantes", label="Python para principiantes"),
+            ft.Radio(value="Python para intermedio", label="Python para intermedio"),
+            ft.Radio(value="Análisis de Datos con Pandas", label="Análisis de Datos con Pandas"),
+        ])
+    ))
+    
+    requiere=page.add(ft.Checkbox(label="Requiere computadora portátil"))
+    
+    nivel=page.add(ft.Slider(label="{value}", value=1, max=5, min=1, divisions=5, width=400))
+    
+    page.add(ft.ElevatedButton(
+    Text="holaaas"
+    ))
+
+
+    
+    def resume():
+        page.add(ft.Text(f"Nombre: {nombre}\n Email:{correo}\n Taller{taller} \n Pago{pago}\n Requiere Portatil: {requiere}\n Nivel:{nivel}\n --- Gracias por su registro ---"))    
+    
+    
+ft.run(main) 
